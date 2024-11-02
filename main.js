614c3fe6df3f6ba9803bf2b5ea7bfca03dbf73ff
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-11-02 22:14:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// const numbers = [1, 3, 7, 8, 10];
+// const first = numbers.find(num => num % 2 === 0);
+// console.log(first); 
+
+
+
+//___________________________
+
+
+
+
+
+
+// const words = ['кот', 'собака', 'тигр', 'слон'];
+// const longWord = words.find(word => word.length > 5);
+// console.log(longWord); 
+
+
+
+
+//________________________
+
+
+
+
+// const numbers = [5, 9, 12, 3, 18];
+// const bolsh10 = numbers.find(num => num > 10);
+// console.log(bolsh10); 
+
+
+
+
+//___________________________________________________
+
+
+
+
+// const users = [
+//     { name: 'Алекс', age: 25 },
+//     { name: 'Мария', age: 17 },
+//     { name: 'Иван', age: 19 }
+//   ];
+//   const userMaria = users.find(user => user.name === 'Мария');
+//   console.log(userMaria); 
+
+
+
+
+
+
+//____________________________________________________
+  
+
+
+
+
+
+
+// const numbers = [3, 5, -2, -8, 7];
+// const otr = numbers.find(num => num < 0);
+// console.log(otr); 
+
